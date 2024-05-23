bc476a6e28c77ebb3fe694d536fd654813d0b64b
maria <medeiroslima.me@gmail.com>
2024-05-23 20:41:05 -0300

    adding serialization

--- curso/APIs/HandlingExcpetions/WritingFiles/Aula1.java
+++ curso/APIs/HandlingExcpetions/WritingFiles/Aula1.java
@@ -1,5 +1,7 @@
 package curso.APIs.HandlingExcpetions.WritingFiles;
 
+import com.google.gson.Gson;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -22,5 +24,14 @@ public class Aula1 {
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
         System.out.println(response.body());
+
+        Gson gson = new Gson();
+        Titulo titulo1 = gson.fromJson(response.body(), Titulo.class);
+        System.out.println("Título: " + titulo1);
+        //getNome() vem nulo. O "zero" de variável de referência para objeto é null
+        //O Json que foi retornado veio com as chaves em inglês, como Title e Year, e não Nome ou Ano de Lançamento.
+        // Usamos anotações na classe Titulo para fazer o "mapeamento"
+        // Serializar é um verbo usado quando transformamos objetos ou estruturas de memória em texto -
+        // não importa se é JSON, binário ou XML.
     }
 }
